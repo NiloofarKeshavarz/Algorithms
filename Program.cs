44a5a5ee15b3ed421195985f1c160c5cb52875d8
Niloofar Keshavarz <niloofar.keshavarz@gmail.com>
2023-09-26 11:10:12 -0400

    max function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static int FindMax(int a, int b, int c)
+    {
+        int max = a;
+
+        if (b > max)
+        {
+            max = b;
+        }
+
+        if (c > max)
+        {
+            max = c;
+        }
+
+        return max;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Maximum value is: " + FindMax(2, 5, 9));
+    }
+}
