cc1c5ce282ae2897c374c56d56e7dd9694eb4f40
Niloofar Keshavarz <niloofar.keshavarz@gmail.com>
2023-09-26 13:49:06 -0400

    Search and parse

--- Program.cs
+++ Program.cs
@@ -18,7 +18,37 @@ class Program
     static string NormalizeString (String input){
         return input.ToLower().Trim().Replace("," ," ");
 
-    }    
+    }  
+
+    //least complex and least efficient method to search: Contain() method
+    //if you need iterate through the whole string :
+    static void ParseContens(string s){
+        Console.WriteLine("the first Option is: ");
+        foreach(char c in s){
+              Console.WriteLine(c);
+        }
+        Console.WriteLine("the second Option is: ");
+        for (int i = 0 ; i <s.Length ; i++){
+            char c = s[i];
+            Console.WriteLine(c);
+        }
+    
+    }
+    static Boolean IsAtEvenIndex(String s , char item){
+        //check the string first 
+        if (String.IsNullOrEmpty(s)){
+            return false;
+        }
+
+        //if Not Empty or Null
+        for (int i = 0 ; i <s.Length / 2 +1 ; i= i+2){
+          if(s[i]== item){
+            return true;
+          }
+        }
+        return false;
+    }
+
 
     static int FindMax(int a, int b, int c)
     {
@@ -43,7 +73,14 @@ class Program
         // Console.WriteLine("the result is " + IsUpperCase("AAA"));
         // Console.WriteLine("the result is " + IsLowerCase("AAA"));
        // Console.WriteLine("the password complexity is " + IsPasswordComplex("aaa24"));
-        Console.WriteLine("the normalized string is: " + NormalizeString("Hello Niloo, Are you ok?       "));
+       // Console.WriteLine("the normalized string is: " + NormalizeString("Hello Niloo, Are you ok?       "));
+
+         //least complex and least efficient method to search: Contain() method
+        // Console.WriteLine("the normalized string is: ".ToLower().Contains("al"));
+        //  ParseContens("");
+
+        String input = " hello ";
+          Console.WriteLine(IsAtEvenIndex(input, 'e'));
     }
     }
 
