355af45e43247570d3cd2e53232c26f96f609302
Niloofar Keshavarz <niloofar.keshavarz@gmail.com>
2023-09-26 11:33:12 -0400

    validation with Any and All

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿using System;
+using System.Linq;
 
 class Program
 {
+
+    static Boolean IsUpperCase (string s){
+        return s.All(char.IsUpper); // All returns a boolean after cheching a certain condition on all elements
+    }
+    static Boolean IsLowerCase (string s){
+        return s.All(char.IsLower); // All returns a boolean after cheching a certain condition on all elements
+    }
+    
+    static Boolean IsPasswordComplex( string s){
+        return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+    } 
+
+
+
+    
+
     static int FindMax(int a, int b, int c)
     {
         int max = a;
@@ -21,6 +38,9 @@ class Program
 
     static void Main(string[] args)
     {
-        Console.WriteLine("Maximum value is: " + FindMax(2, 5, 9));
+        // Console.WriteLine("Maximum value is: " + FindMax(2, 5, 9));
+        // Console.WriteLine("the result is " + IsUpperCase("AAA"));
+        // Console.WriteLine("the result is " + IsLowerCase("AAA"));
+        Console.WriteLine("the password complexity is " + IsPasswordComplex("aaa24"));
     }
 }
